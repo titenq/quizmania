b4134bec8cc176851e4fde2e8285d6e69e909495
TitenQ <titenq@gmail.com>
2024-06-19 12:18:00 -0300

    css variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
 
-const clientId = import.meta.env.VITE_CLIENT_ID;
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <GoogleOAuthProvider clientId={clientId}>
-    <AuthProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </AuthProvider>
-  </GoogleOAuthProvider>
+  <AuthProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthProvider>
 );
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,25 +1,11 @@
-// import { useNavigate } from 'react-router-dom';
-
-// import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import { FaFacebook, FaGithub, FaGoogle } from 'react-icons/fa';
 
 import styles from './Login.module.css';
-// import { useAuth } from '../../hooks/useAuth';
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const githubClientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
 
 const Login = () => {
-  /* const { loginGoogle, logout } = useAuth();
-  const navigate = useNavigate(); */
-
-  /* const handleGoogleLogin = (credentialResponse: CredentialResponse) => {
-    const credential = credentialResponse.credential;
-    credential && loginGoogle(credential);
-
-    return navigate('/admin');
-  }; */
-
   const handleGoogleLogin = () => {
     const redirectUri = 'http://localhost:4000/google/callback';
 
@@ -27,12 +13,6 @@ const Login = () => {
 
     window.location.href = googleAuthUrl;
   };
-
-  /* const handleGoogleOnError = () => {
-    logout();
-
-    return navigate('/');
-  }; */
 
   const handleGithubLogin = () => {
     const redirectUri = 'http://localhost:4000/github/callback';
@@ -48,11 +28,6 @@ const Login = () => {
 
   return (
     <div className={styles.container}>
-      {/* <GoogleLogin
-        onSuccess={handleGoogleLogin}
-        onError={handleGoogleOnError}
-      /> */}
-
       <button
         onClick={handleGoogleLogin}
         className={`${styles.loginButton} ${styles.googleButton}`}
