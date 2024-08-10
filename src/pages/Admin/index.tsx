3a531e14bfbe14b5a93c6b4dca82b382d3a83bf0
titenq <titenq@gmail.com>
2024-08-09 21:18:22 -0300

    pagination

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -6,7 +6,7 @@ import { FaCheckCircle, FaPlusCircle, FaLink, FaTimesCircle, FaQuestionCircle } 
 import styles from './Admin.module.css';
 import AuthContext from '../../context/AuthContext';
 import getAllQuizByUserId from '../../api/quiz/getAllQuizByUserId';
-import { IQuizzes } from '../../interfaces/IQuiz';
+import { IQuizResponse, IQuizzes } from '../../interfaces/IQuiz';
 import formatDate from '../../helpers/formatDate';
 import Button from '../../components/Button';
 import { IAnswersPercentage } from '../../interfaces/IAnswer';
@@ -15,7 +15,9 @@ import getAnswersPercentage from '../../api/answer/getAnswersPercentage';
 const Admin = () => {
   const navigate = useNavigate();
   const { user } = useContext(AuthContext);
-  const [quizzes, setQuizzes] = useState<IQuizzes | null>(null);
+  const [quizzes, setQuizzes] = useState<IQuizResponse[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const [answersPercentage, setAnswersPercentage] = useState<IAnswersPercentage[]>([]);
 
   const creatingQuiz = () => {
@@ -26,11 +28,15 @@ const Admin = () => {
     const getAllQuiz = async (userId: string, page: number) => {
       const response: IQuizzes = await getAllQuizByUserId(userId, page);
 
-      response && setQuizzes(response);
+      console.log(response)
+
+      setQuizzes(response.quizzes);
+      setCurrentPage(response.currentPage);
+      setTotalPages(response.totalPages);
     };
 
-    user && getAllQuiz(user._id, 1);
-  }, [user]);
+    user && getAllQuiz(user._id, currentPage);
+  }, [currentPage, user]);
 
   useEffect(() => {
     const fetchAnswersPercentage = async (userId: string, page: number) => {
@@ -48,6 +54,10 @@ const Admin = () => {
     );
   };
 
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.title_container}>
@@ -61,13 +71,13 @@ const Admin = () => {
         />
       </div>
 
-      {quizzes?.quizzes?.length === 0 && (
+      {quizzes?.length === 0 && (
         <p className={`${styles.no_quizzes} ${styles.neumorphism}`}>
           Você ainda não criou nenhum Quiz, clique em CRIAR QUIZ, é fácil e rápido!
         </p>
       )}
 
-      {quizzes?.quizzes?.length !== 0 && (
+      {quizzes?.length !== 0 && (
         <table className={styles.neumorphism_table}>
           <thead>
             <tr>
@@ -79,7 +89,7 @@ const Admin = () => {
           </thead>
 
           <tbody>
-            {quizzes?.quizzes.map((quiz, index) => (
+            {quizzes.map((quiz, index) => (
               <tr key={quiz._id}>
                 <td>{quiz.quizTitle}</td>
                 <td style={{ textAlign: 'center' }}>{formatDate(quiz.createdAt)}</td>
@@ -113,6 +123,30 @@ const Admin = () => {
           </tbody>
         </table>
       )}
+
+      <div className={styles.pagination}>
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Anterior
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => handlePageChange(index + 1)}
+            className={currentPage === index + 1 ? styles.activePage : ''}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Próximo
+        </button>
+      </div>
     </div>
   );
 };
