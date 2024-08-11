2418dd1c9126fe866e64cf21c8ff9cf3b7c0d673
titenq <titenq@gmail.com>
2024-08-11 19:57:31 -0300

    base url

--- src/helpers/baseUrl.ts
+++ src/helpers/baseUrl.ts
@@ -4,7 +4,7 @@ let frontendBaseUrl = 'http://localhost:5173';
 let backendBaseUrl = 'http://localhost:4000';
 
 if (env === 'PROD') {
-  frontendBaseUrl = import.meta.env.VITE_FRONTEND_BASE_URL;
+  frontendBaseUrl = import.meta.env.VITE_BASE_URL;
   backendBaseUrl = import.meta.env.VITE_BACKEND_BASE_URL;
 }
 
