507bda7a08d1ee8b7a6c08af44e1ed856657ee0a
titenq <titenq@gmail.com>
2024-08-11 22:38:48 -0300

    google analytics tag

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import ReactGA from 'react-ga4';
+
 import App from './App.tsx';
 import './index.css';
 import AuthProvider from './context/AuthProvider.tsx';
+
+const GOOGLE_ANALYTICS_TAG = import.meta.env.VITE_GOOGLE_ANALYTICS_TAG;
+
+ReactGA.initialize(GOOGLE_ANALYTICS_TAG);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AuthProvider>
